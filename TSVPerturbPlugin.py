32e979d1b129f1d76cd84301b655a0ffe7749e54
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2023-07-28 15:50:20 -0400

    Perturb amt

--- TSVPerturbPlugin.py
+++ TSVPerturbPlugin.py
@@ -2,7 +2,7 @@ import sys
 
 class TSVPerturbPlugin:
     def input(self, filename):
-       self.infile = open(filename, 'r')
+        self.infile = open(filename, 'r')
 
     def run(self):
         pass
@@ -19,7 +19,8 @@ class TSVPerturbPlugin:
           outfile.write('\t')
           for i in range(1,len(contents)):
               if (float(contents[i]) == 0):
-                  outfile.write("0.000000001")
+                  outfile.write("0.01")
+                  #outfile.write("0.000000001")
               else:
                   outfile.write(contents[i])
               if (i == len(contents)-1):
